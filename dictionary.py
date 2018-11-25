78067583357e6d2676aabf8578830882f2a8f1c6
Abdillahi-A <39855630+Abdillahi-A@users.noreply.github.com>
2018-11-25 19:15:49 +0000

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,39 +1,43 @@
-import  requests
+import requests
 
-app_id = 'YOUR API ID'
-app_key = 'YOUR API KEY'
-language = 'en'
-word_id = input("Enter a word: ")
-url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/'  + language + '/'  + word_id.lower()
-#url Normalized frequency
-urlFR = 'https://od-api.oxforddictionaries.com:443/api/v1/stats/frequency/word/'  + language + '/?corpus=nmc&lemma=' + word_id.lower()
+def getWord():
+    word_id = input("Enter a word: ")
+    return word_id
 
-r = requests.get(url, headers = {'app_id' : app_id, 'app_key' : app_key})
-
-while r.status_code == 404:
-    print("Sorry, word not found.")
-    word_id = input("Pleas enter another word: ")
+def getAPIdata(word_id):
+    app_id = 'YOUR APP_ID'
+    app_key = 'YOUR APP_KEY'
+    language = 'en'
     url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/'  + language + '/'  + word_id.lower()
     #url Normalized frequency
     urlFR = 'https://od-api.oxforddictionaries.com:443/api/v1/stats/frequency/word/'  + language + '/?corpus=nmc&lemma=' + word_id.lower()
+
     r = requests.get(url, headers = {'app_id' : app_id, 'app_key' : app_key})
-    
-else:
+
+    return r
+
+def displayResults(word_id, data):
+    print(word_id.capitalize()+':')
+    #prints Phonetic Spelling
+    print('Phonetic Spelling:\n-{}'.format(data['results'][0]['lexicalEntries'][0]['pronunciations'][0]['phoneticSpelling']))
+    #prints parts of speech
+    for i in range(len((data['results'][0]['lexicalEntries']))):
+        print('Parts of Speech:\n-{}'.format(data['results'][0]['lexicalEntries'][i]['lexicalCategory']))
+        print("Definition:")
+        #prints each of the definitions of the word.
+        for j in range(len(data['results'][0]['lexicalEntries'][i]['entries'])):
+            print('{}. {}'.format((j+1),data['results'][0]['lexicalEntries'][i]['entries'][j]['senses'][0]['definitions'][0]))
+ 
+
+def main():
+    word_id = getWord()
+    r = getAPIdata(word_id)
+    while r.status_code == 404:
+        print("Sorry, word not found.")
+        word_id = getWord()
+        r = getAPIdata(word_id)
     data = r.json()
+    displayResults(word_id, data)
+    
 
-    definition = data['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
-    examples = data['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['examples']
-    parts_of_speech = data['results'][0]['lexicalEntries'][0]['lexicalCategory']
-    phoneticSpelling = data['results'][0]['lexicalEntries'][0]['pronunciations'][0]['phoneticSpelling']
-
-    def show_examples(examples):
-        for i in range(len(examples)):
-            print('"{}"'.format(examples[i]['text']))
-
-    print('Phonetic Spelling:\n-{}'.format(phoneticSpelling))
-    print("Parts of Speech:\n-{}".format(parts_of_speech))
-    print("Definition:\n-" + definition)
-    print("Examples:")
-    show_examples(examples)
-
-#we get a KeyError: 'examples' when ninja is entered. try and figure this out.
+main()
